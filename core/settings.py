e6e174a3bfbcaea02f9f21bd14a7d713ca6d14ec
Amrit Dhakal <70811728+dhakal9@users.noreply.github.com>
2024-01-26 21:45:14 +0545

    Revert "working on language"

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': 'goreto_treks',  
         'USER': 'root',  
         'PASSWORD': '',  
-        'HOST': 'localhost',  
+        'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
